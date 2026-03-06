57afbcf9972ddb653a4a18e1558b7d4e50a67296
KSHITIZ DIXIT <kshitizd171@gmail.com>
2026-03-06 13:36:06 +0530

    Final draft decided.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  // State to track when the 3D intro is done (either settles or user skips)
   const [introFinished, setIntroFinished] = useState(false);
 
   return (
     <div className="app" style={{ position: 'relative', minHeight: '100vh' }}>
       
-      {/* 3D Background Wrapper */}
+      {/* BULLETPROOF WRAPPER:
+        Using inline styles to guarantee it takes exactly 100% of your screen width and height.
+      */}
       <div style={{ 
         position: 'fixed', 
         top: 0, 
@@ -25,18 +26,15 @@ function App() {
           camera={{ position: [0, -2, 15], fov: 50 }}
           style={{ width: '100%', height: '100%', display: 'block' }}
         >
-          {/* We bumped the count to 10k and added the trigger! */}
-          <BigBangTree 
-            count={10000} 
-            onAnimationComplete={() => setIntroFinished(true)} 
-          />
+          {/* A visual grid on the floor (y = -5) to help us debug 3D space */}
+          <gridHelper args={[50, 50, '#FF2E63', '#444444']} position={[0, -5, 0]} />
+          
+          <BigBangTree count={2000} />
         </Canvas>
       </div>
 
-      {/* UI Foreground Wrapper */}
       <div style={{ position: 'relative', zIndex: 10, width: '100%', height: '100%' }}>
-        {/* We pass the introFinished signal down to your Home component */}
-        <Home introFinished={introFinished} />
+        <Home />
       </div>
       
     </div>
